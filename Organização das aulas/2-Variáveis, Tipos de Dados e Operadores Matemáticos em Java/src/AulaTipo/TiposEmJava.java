b92a21840225453fb30518077202ec414507e630
Pedrosilva14 <pedro-hslv@hotmail.com>
2021-12-18 14:27:41 -0300

    Atualizado em 18/12/2021

---
+++ Organização das aulas/2-Variáveis, Tipos de Dados e Operadores Matemáticos em Java/src/AulaTipo/TiposEmJava.java
@@ -0,0 +1,33 @@
+package AulaTipo;
+
+public class TiposEmJava {
+
+    public static void main(String[] args) {
+
+        // jáva é queise sencintive ,não pode começar variáves com numero
+        //variáveis dentro de metodos devem ser iniciadas
+        // "final" é um valor constante
+        // caractereis especias não podem ser usados
+        //devo conhecer as boas praticas de java e os timpos de java , além dos temos que não podem ser usados
+        //tenho que colocar um f no fila no baçpr da variável para dizer que é um float e b para dizer que é um double
+        // usasse aspas simples para um caracter isolada ' ' , se for String é dupla ""
+        //objeto é um  tipo
+        // é uma boa pratica de programação usar cada tipo de dado para cada informação de forma adequada
+        //estudar as tabelas de conversão de java
+
+
+        int i;
+        int I;
+        final int j =10;
+        i = 10;
+        I = 11;
+
+        System.out.println("O valor de i " + i );
+        System.out.println("O valor de I " + I );
+        System.out.println("O valor de J " + j );
+
+
+
+    }
+
+}
---
+++ Organização das aulas/3-Lógica Condicional e Controle de Fluxos em Java/src/ControleDeFluxo.java
@@ -0,0 +1,47 @@
+public class ControleDeFluxo {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=12; i++){
+
+            if (i==1){
+
+                System.out.println(" janeiro " + i);
+            }
+
+            if (i==2){
+
+                System.out.println(" fevereiro " + i);
+            }
+
+            if (i==3){
+
+                System.out.println(" março " + i);
+            }
+
+            if (i==4){
+
+                System.out.println(" abril " + i);
+            }
+
+            if (i==12){
+
+                System.out.println(" Dezembro é  Mês de ferias " + i);
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+}
